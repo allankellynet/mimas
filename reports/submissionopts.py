e61d35011640b97ac3ce0e9246d0923dda5d56f0
allan kelly <allan@allankelly.net>
2020-02-03 14:23:40 +0000

    Refactoring to reduce code

--- reports/submissionopts.py
+++ reports/submissionopts.py
@@ -13,44 +13,29 @@
 # Local imports
 from submission_lib import submissionrecord
 
-def write_created(sub):
-    return str(sub.created)
-
-def write_boolean(flag):
+def write_str(flag):
     return str(flag)
-
-def write_track(sub):
-    return sub.key.parent().get().track_options()[sub.track]
-
-def write_duration(sub):
-    return sub.key.parent().get().duration_options()[sub.duration]
 
 def write_decision(sub, review_round):
     return sub.review_decision(review_round)
 
-def write_format(sub):
-    return sub.key.parent().get().delivery_format_options()[sub.delivery_format]
-
-def write_expenses(sub):
-    return sub.key.parent().get().expenses_options()[sub.expenses]
-
-def write_option(sub, option_list, opt):
+def write_option(option_list, opt):
     return option_list[opt]
-
-def write_comms(sub):
-    return sub.communication
 
 #                      Key -> ("Description", writer_func)
 submission_options = {
-    "created": ("Date and time created", write_created),
-    "track": ("Track", write_track),
-    "format": ("Format", write_format),
+    "created": ("Date and time created", lambda sub:write_str(sub.created)),
+    "track": ("Track",
+        lambda sub:write_option(sub.key.parent().get().track_options(), sub.track)),
+    "format": ("Format",
+        lambda sub:write_option(sub.key.parent().get().delivery_format_options(), sub.delivery_format)),
     "decision1": ("Decision round 1", lambda sub:write_decision(sub, 1)),
     "decision2": ("Decision round 2", lambda sub:write_decision(sub, 2)),
-    "duration": ("Length", write_duration),
-    "speaker_comms": ("Communication", write_comms),
+    "duration": ("Length",
+        lambda sub:write_option(sub.key.parent().get().duration_options(), sub.duration)),
+    "speaker_comms": ("Communication", lambda sub:write_str(sub.communication)),
     "expenses": ("Expenses",
-                lambda sub:write_option(sub, sub.key.parent().get().expenses_options(), sub.expenses)),
-    "withdrawn": ("Withdrawn", lambda sub:write_boolean(sub.withdrawn)),
-    "grdp_agreed": ("Agreed GDPR policy", lambda sub:write_boolean(sub.gdpr_agreed_flag)),
+        lambda sub:write_option(sub.key.parent().get().expenses_options(), sub.expenses)),
+    "withdrawn": ("Withdrawn", lambda sub:write_str(sub.withdrawn)),
+    "grdp_agreed": ("Agreed GDPR policy", lambda sub:write_str(sub.gdpr_agreed_flag)),
     }
