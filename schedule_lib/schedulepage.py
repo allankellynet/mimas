0930b2349716b57603df3c285f5072f74956fc8f
allan kelly <allan@allankelly.net>
2020-02-11 16:40:49 +0000

    More scheudle

--- schedule_lib/schedulepage.py
+++ schedule_lib/schedulepage.py
@@ -15,6 +15,7 @@ from google.appengine.ext import ndb
 # Local imports
 import basehandler
 from conference_lib import conference
+from submission_lib import submissionrecord
 from schedule_lib import schedule
 
 class SchedulePage(basehandler.BaseHandler):
@@ -27,8 +28,18 @@ class SchedulePage(basehandler.BaseHandler):
         else:
             selected_day = ""
 
+        crrt_conference=conf_key.get()
+        tracks = crrt_conference.track_options()
+
+        submissions = {}
+        for t in tracks:
+            submissions[t] = submissionrecord.retrieve_conference_submissions_by_track_round_and_decision(
+                                conf_key, t, 2, "Accept")
+
         self.write_page('schedule_lib/schedulepage.html',
                         { "sched": sched.get(),
                           "selectedDay": selected_day,
-                        })
-
+                          "conf_tracks": tracks,
+                          "submissions": submissions,
+                          "crrt_conference": crrt_conference,
+                          })
